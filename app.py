0d311934c718a35c9db3349ada4583574d27f3ba
Namrata-m-aurya <84679860+Namrata-m-aurya@users.noreply.github.com>
2021-12-08 22:01:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,24 @@
+from flask import Flask
+from flask.templating import render_template
+app = Flask(__name__)
 
+@app.route('/')
+def home():
+    user={'username':'namrata'}
+    return render_template('home.html', user=user)
+
+@app.route('/about')
+def about():
+    posts=['hello','post1','hello2']
+    return render_template('about.html', posts=posts)
+
+@app.route('/login')
+def userLogin():
+    return render_template('login.html')
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
