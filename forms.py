72436d77672c74b90a13dabba1ca4bcad7e5087d
shvenktesh9 <39987441+shvenktesh9@users.noreply.github.com>
2021-12-09 16:17:31 +0530

    Update forms.py

--- forms.py
+++ forms.py
@@ -1,1 +1,17 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import InputRequired, Email, DataRequired, EqualTo
 
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[InputRequired(), Email()])
+    password = PasswordField('Password', validators=[InputRequired()])
+    submit = SubmitField('Login')
+
+
+class SignUpForm(FlaskForm):
+    full_name = StringField('Full Name')
+    email = StringField('Email')
+    password = PasswordField('Password', validators=[InputRequired()])
+    confirm_password = PasswordField('Confirm', validators=[EqualTo('password'), InputRequired()])
+    submit = SubmitField('Sign Up')
