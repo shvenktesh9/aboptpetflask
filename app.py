6492cbc1b037efadd6010fb146c2fc95bcf668b0
shvenktesh9 <39987441+shvenktesh9@users.noreply.github.com>
2021-12-10 18:14:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,4 @@
-from flask import Flask,render_template
-from forms import SignUpForm , LoginForm
-from flask import session, redirect, url_for
-from flask_sqlalchemy import SQLAlchemy
-
 app = Flask(__name__)
-
 app.config['SECRET_KEY'] = 'dfewfew123213rwdsgert34tgfd1234trgf'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///paws.db'
 db = SQLAlchemy(app)
@@ -26,31 +20,79 @@ class User(db.Model):
     password = db.Column(db.String)
     pets = db.relationship('Pet', backref = 'user')
 
-db.create_all() 
+db.create_all()
 
-pets=[{"id":1, "name":"Rocky", "description":"German Shephard"}]
-users = [
-            {"id": 1, "full_name": "Pet", "email": "t@pets.co", "password": "password"},
-        ]
-@app.route('/')
-def home():
-    user={'username':'namrata'}
-    return render_template('home.html', user=user, pets=pets)
+team = User(full_name = "Pet", email = "t@pets.co", password = "password")
+db.session.add(team)
 
-@app.route('/about')
+
+@app.route("/")
+def homepage():
+    pets = Pet.query.all()
+    return render_template("home.html", pets = pets)
+
+
+@app.route("/about")
 def about():
-    posts=['hello','post1','hello2']
-    return render_template('about.html', posts=posts)
+    return render_template("about.html")
+
+
+@app.route("/details/<int:pet_id>", methods=["POST", "GET"])
+def pet_details(pet_id):
+    form = EditPetForm()
+    pet = Pet.query.get(pet_id)
+    if pet is None: 
+        abort(404, description="No Pet was Found with the given ID")
+    if form.validate_on_submit():
+        pet.name = form.name.data
+        pet.age = form.age.data
+        pet.bio = form.bio.data
+        try:
+            db.session.commit()
+        except Exception as e:
+            db.session.rollback()
+            return render_template("details.html", pet = pet, form = form, message = "A Pet with this name already exists!")
+    return render_template("details.html", pet = pet, form = form)
+
+@app.route("/delete/<int:pet_id>")
+def delete_pet(pet_id):
+    pet = Pet.query.get(pet_id)
+    if pet is None: 
+        abort(404, description="No Pet was Found with the given ID")
+    db.session.delete(pet)
+    try:
+        db.session.commit()
+    except Exception as e:
+        db.session.rollback()
+    return redirect(url_for('homepage', _scheme='https', _external=True))
+
+@app.route("/signup", methods=["POST", "GET"])
+def signup():
+    form = SignUpForm()
+    if form.validate_on_submit():
+        new_user = User(full_name = form.full_name.data, email = form.email.data, password = form.password.data)
+        db.session.add(new_user)
+        try:
+            db.session.commit()
+        except Exception as e:
+            print(e)
+            db.session.rollback()
+            return render_template("signup.html", form = form, message = "This Email already exists in the system! Please Login instead.")
+        finally:
+            db.session.close()
+        return render_template("signup.html", message = "Successfully signed up")
+    return render_template("signup.html", form = form)
+
 
 @app.route("/login", methods=["POST", "GET"])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        user = next((user for user in users if user["email"] == form.email.data and user["password"] == form.password.data), None)
+        user = User.query.filter_by(email = form.email.data, password = form.password.data).first()
         if user is None:
             return render_template("login.html", form = form, message = "Wrong Credentials. Please Try Again.")
         else:
-            session['user'] = user
+            session['user'] = user.id
             return render_template("login.html", message = "Successfully Logged In!")
     return render_template("login.html", form = form)
 
@@ -59,23 +101,6 @@ def logout():
     if 'user' in session:
         session.pop('user')
     return redirect(url_for('homepage', _scheme='https', _external=True))
-
-@app.route("/signup",methods=["GET","POST"])
-def Signup():
-    form = SignUpForm()
-    if form.validate_on_submit():
-        new_user = {"id": len(users)+1, "full_name": form.full_name.data, "email": form.email.data, "password": form.password.data}
-        users.append(new_user)
-        return render_template("signup.html", message = "Successfully signed up")
-    return render_template("signup.html", form = form)
-
-@app.route('/details/<int:id>')
-def details(id):
-    if pets[0]["id"]==id:
-        return render_template('details.html', id=id, pet=pets[0])
     
-
-
-
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0", port=3000)
