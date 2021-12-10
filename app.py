f836522115cc2123f25f47da5dc9604ab9107743
shvenktesh9 <39987441+shvenktesh9@users.noreply.github.com>
2021-12-10 18:22:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
+from flask import Flask, render_template, abort
+from forms import SignUpForm, LoginForm, EditPetForm
+from flask import session, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'dfewfew123213rwdsgert34tgfd1234trgf'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///paws.db'
